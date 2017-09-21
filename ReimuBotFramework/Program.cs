987ee7c8f16dd8eb9ee9c297ae3691db989229d2
KelaKim <KelaKim@users.noreply.github.com>
2017-09-21 13:22:27 +0800

    添加项目文件。

---
+++ ReimuBotFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReimuBotFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
