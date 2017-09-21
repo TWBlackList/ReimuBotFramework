6248b59bf21b218c5407dc5fd4a2025d8c5a85f4
KelaKim <KelaKim@users.noreply.github.com>
2017-09-21 13:34:15 +0800

    First commit

---
+++ ReimuBotFramework/ReimuBase/BotFramework.cs
@@ -0,0 +1,54 @@
+﻿using ReimuAPI.ReimuBase;
+using ReimuAPI.ReimuBase.Caller;
+using ReimuAPI.ReimuBase.TgData;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading;
+
+namespace ReimuBotFramework.ReimuBase
+{
+    public class BotFramework
+    {
+        public BotFramework()
+        {
+            RAPI.loadPlugins();
+        }
+
+        internal void NewRequest(string JsonMessage)
+        {
+            TgBaseMessage data = null;
+            try
+            {
+                data = (TgBaseMessage)new DataContractJsonSerializer(
+                    typeof(TgBaseMessage)
+                ).ReadObject(
+                    new MemoryStream(
+                        Encoding.UTF8.GetBytes(JsonMessage)
+                    )
+                );
+            }
+            catch (System.Runtime.Serialization.SerializationException e)
+            {
+                RAPI.GetExceptionListener().OnJsonDecodeError(e, JsonMessage);
+                return;
+            }
+            CallPlugins(data, JsonMessage);
+            return;
+        }
+
+        private void CallPlugins(TgBaseMessage message, string JsonMessage)
+        {
+            if (message.message != null) // 正常的消息
+            {
+                new Thread(delegate () {
+                    try
+                    {
+                        new NormalMessageCaller().call(message.message, JsonMessage);
+                    }
+                    catch (StopProcessException) { }
+                }).Start();
+            }
+        }
+    }
+}
---
+++ ReimuBotFramework/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using ReimuBotFramework.ReimuBase;
+using System;
+
+namespace ReimuBotFramework
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            BotFramework botFramework = new BotFramework();
+            app.Run(async (context) =>
+            {
+                context.Response.ContentType = "text/plain";
+                string filePath = context.Request.Path;
+                if (filePath.Length >= 16)
+                {
+                    string jsonString = new StreamReader(context.Request.Body).ReadToEnd();
+                    string path = filePath.Substring(0, 16).ToLower();
+                    if (path == "/teleapireceiver")
+                    {
+                        botFramework.NewRequest(jsonString);
+                    } else if (path == "/pluginsreceiver")
+                    {
+                        // TODO: 加入插件的 WebHook 支持
+                    }
+                }
+                await context.Response.WriteAsync("a");
+            });
+        }
+    }
+}
